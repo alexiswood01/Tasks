edfc5f79795eea3f500c0e08be8f2fa1dbd75cd3
Lexy Wood <lexywood@Lexys-MacBook-Air-2.local>
2022-01-13 14:34:46 -0500

    Alexis Wood Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,4 +1,29 @@
-#  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
+# password for git via terminal  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
 
-install.packages ("swirl")
+#install.packages ("swirl")
 library (swirl)
+dir.create(file.path("~", "swirl", "courses"), recursive = TRUE)
+dir.create(file.path("~", "swirl", "data"), recursive = TRUE)
+swirl_options(swirl_courses_dir = file.path("~", "swirl", "courses"), swirl_data_dir = file.path("~", "swirl", "data"))
+install_course("R Programming")
+1
+x <- 5+7
+x
+y <- x-3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, 5)
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z - 1)
+2
+my_sqrt
+my_div <- z/my_sqrt
+2
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+my_div
+2
