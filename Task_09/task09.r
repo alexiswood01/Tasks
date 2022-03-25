acc6677dea04cbba2e64f95ffd014df431ae070c
alexiswood01 <adw0037@mix.wvu.edu>
2022-03-25 09:47:28 -0400

    Alexis Wood Task 09

--- Task_09/task09.r
+++ Task_09/task09.r
@@ -14,8 +14,8 @@ svl
 Ancestors <- fastAnc(tree, svl, vars=TRUE, CI=TRUE)
 Ancestors
 ?fastAnc
-#Q3 (CI95) is the 95-percent confidence interval. 
-#Q4
+#Q3 (CI95) is the 95-percent confidence interval. estimated values are stored in the node. 
+#Q4 Assumes the state compund for the root node = MLE. Assumes there is a 95% confidence interval
 par(mar=c(0.1,0.1,0.1,0.1))
 plot(tree, type="fan", lwd=2, show.tip.label=F)
 tiplabels(pch=16, cex=0.25*svl[tree$tip.label])
@@ -25,6 +25,39 @@ plot(obj, type="fan", legend=0.7*max(nodeHeights(tree)), sig=2, fsize=c(0.7, 0.9
 fossilData <- data.frame(svl=log(c(25.4, 23.2, 17.7, 19.7, 24, 31)), tip1=c(
   "Anolis_aliniger", "Anolis_aliniger", "Anolis_occultus", "Anolis_ricordii", 
   "Anolis_cristatellus", "Anolis_occultus"), tip2=c("Anolis_chlorocyanus", "Anolis_coelestinus", "Anolis_hendersoni", "Anolis_cybotes", "Anolis_angusticeps", "Anolis_angusticeps"))
+fossilNodes <- c()
+nodeN <- c()
+head(fossilData)
+for(i in 1:6) {
+  Node <- fastMRCA(tree, fossilData[i, "tip1"], fossilData[i, "tip2"])
+  fossilNodes[i] <- fossilData[i, "svl"]
+  nodeN[i]<- Node
+}
+i
+names(fossilNodes)<- nodeN
+Ancestors_withFossils <- fastAnc(tree, svl, anc.states=fossilNodes, CI=TRUE, var=TRUE)
+Ancestors_withFossils
+#Q7 Fossils increased the estimated ancestral time. 
+#Q8-10 The ebFit model fits the data the best using evidence of the lowest AIC.
+# fitContinuous compares continuous comparative data while fastAnc performs a 
+# fast estimate of ML ancestral states. Both assume continuous traits and 95% CI.
+install.packages("gieger")
+library("gieger")
+deltaFit <- fitContinuous(tree, data, model= "delta") #AIC= -6.106526
+lambdaFit <- fitContinuous(tree, data, model= "lambda") #AIC= -4.512027
+kappaFit <- fitContinuous(tree, data, model="kappa") #AIC= -4.512027
+OuFit <- fitContinuous(tree, data, model="OU") #AIC= -4.512027
+ebFit <- fitContinuous(tree, data, model="EB") #AIC= -7.235124
+brownFit <- fitContinuous(tree, data) #AIC= -6.512027
+meantrendfit <- fitContinuous(tree, data, model="mean_trend") #AIC= -4.512027
+whiteFit <- fitContinuous(tree, data, model="white") #AIC= 91.391102
+ratetrendfit <- fitContinuous(tree, data, model="rate_trend") #AIC= -6.981365
+
+
+
+
+
+
 
 
 
